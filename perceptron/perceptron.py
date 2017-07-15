1996cb776c56ac5beee50452df58845be157cd5e
LookRain <brantscube@hotmail.com>
2017-07-16 02:21:13 +0800

    clean up code

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-# plt.plot([1,2,3,4])
-# plt.ylabel('some numbers')
-# plt.show()
 
 MAX_ITER = 100
 LEARNING_RATE = 0.1
 NUM_INSTANCES = 100
 theta = 0
+
+
 def graph(formula, x_range, c, l):  
     x = np.array(x_range)  
     y = eval(formula)
@@ -19,9 +18,6 @@ def main():
   x = []
   y = []
   truth = []
-
-  # for ind, item in enumerate(x):
-  #   x[ind] = random.uniform(0, 10)
 
   for i in range(50):
     x.append(random.uniform(0, 10))
@@ -36,24 +32,24 @@ def main():
   weights = [random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)]
 
   iteration = 0
-  # print(cal_output(0, [1,2,3], 1, 2))
+
   local_error = 0
   global_error = 0
   output = 0
   
   while(True):
-   
     iteration += 1
     global_error = 0
 
     for ind in range(NUM_INSTANCES):
       output = cal_output(theta, weights, x[ind], y[ind])
       local_error = truth[ind] - output
-      # print(local_error)
+
       weights[0] += LEARNING_RATE * local_error * x[ind]
       weights[1] += LEARNING_RATE * local_error * y[ind]
       weights[2] += LEARNING_RATE * local_error
       global_error += (local_error * local_error)
+
     print("RMS Error: %n", global_error)
     equation = "-({}/{})*x-{}/{}"
     final_equation = equation.format(weights[0], weights[1], weights[2], weights[1])
@@ -61,7 +57,6 @@ def main():
     if (global_error == 0 or iteration > MAX_ITER):
       break
 
-      
   str = "{}*x + {}*y + {} = 0"
   print(str.format(weights[0], weights[1], weights[2]))
   equation = "-({}/{})*x-{}/{}"
@@ -69,20 +64,11 @@ def main():
   print(final_equation)
   graph(final_equation, range(0,20), 'red', 'final')
 
-    # if (test == 10):
-    #   break
-    # test += 1
-    # print(test)
-
-
-  
-
-
-
   plt.plot(x, y, 'ro')
   plt.axis([0, 20, 0, 20])
   plt.legend()
   plt.show()
+
 
 def cal_output(t, w, x, y):
   sum = x * w[0] + y * w[1] + w[2]
