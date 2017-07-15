5dc8b4fd1de938e7822d9f6e2cd779c4b7e07a45
LookRain <brantscube@hotmail.com>
2017-07-15 13:04:00 +0800

    init perceptron constants

---
+++ perceptron/perceptron.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.text.*;
+import java.math.*;
+
+Class Perception {
+	
+	int MAX_ITER = 100;
+	int NUM_INSTANCES = 100;
+	int theta = 0;
+	double LEARNING_RATE = 0.1;
+
+	static void main() {
+
+	}
+}
